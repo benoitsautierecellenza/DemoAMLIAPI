389f5d58c37b784fee446a5a6c8350c582282d98
SAUTIERE Benoit-EXT <benoit.sautiere-ext@alstomgroup.com>
2025-04-10 22:41:19 +0200

    Code cleanup & publish.

--- 0-Introduction/demologingestion.ps1
+++ 0-Introduction/demologingestion.ps1
@@ -6,7 +6,7 @@
 # Version : 1.0
 # Date : 2023-10-03
 #
-# Example : . .\demologingestion.ps1 -ResourceGroup_name "DemoLogIngestion"  -MetricValue 100
+# Example : . .\demologingestion.ps1 -ResourceGroup_name "DemoLogIngestion"  -MetricValue (Get-Random -Minimum 0 -Maximum 100)
 #
 using namespace System.Net
 #Requires -PSedition Core
@@ -17,70 +17,17 @@ param(
     [ValidateNotNullOrEmpty()]
     [String]$ResourceGroup_name,
     [Parameter(mandatory = $True)]
-    [ValidateRange(0,100)]
+    [ValidateRange(0, 100)]
     [int]$MetricValue
 
 )
-
-Function Publish_Custom_Metric(
-    [Parameter(mandatory = $True)]
-    [String]$CommputerName,
-    [Parameter(mandatory = $True)]
-    [String]$Counter_Name,
-    [Parameter(mandatory = $True)]
-    [Int]$Counter_value,
-    [Parameter(mandatory = $True)]
-    [String]$AzureRegion,
-    [Parameter(mandatory = $True)]
-    [Int]$Level    
-) {
-    #
-    # Get Access token from current context
-    #
-    Add-Type -AssemblyName System.Web
-    # Decode SecureString : https://learn.microsoft.com/en-us/powershell/azure/faq?view=azps-13.3.0#how-can-i-convert-a-securestring-to-plain-text-in-powershell-
-    $SecureString = (Get-AzAccessToken  -ResourceUrl "https://monitor.azure.com/" -AsSecureString -WarningAction SilentlyContinue).Token
-    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
-    $BearTokenplainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-    $currentTime = Get-Date ([datetime]::UtcNow) -Format O
-    $body = @"
-[
-{
-    "Time": "$currentTime",
-    "Computer": "$CommputerName",
-    "AdditionalContext": {
-        "InstanceName": "$AzureRegion",
-        "TimeZone": "$WebSite_Time_Zone",
-        "Level": $Level,
-        "CounterName": "$Counter_Name",
-        "CounterValue": "$Counter_value"    
-    }
-}
-]
-"@;
-    # https://learn.microsoft.com/en-us/azure/azure-monitor/logs/logs-ingestion-api-overview
-    try {
-        $headers = @{"Authorization" = "Bearer $BearTokenplainText"; "Content-Type" = "application/json" }
-        $uri = "$dceEndpoint/dataCollectionRules/$dcrImmutableId/streams/$($streamName)?api-version=2021-11-01-preview"
-        $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers   
-    }
-    catch {
-        [String]$ErrorMessage = "Error while publishing custom metrics using uri $uri : $($_.Exception.Message)."
-        Write-Error $ErrorMessage
-        return $null
-    }
-    $BearTokenplainText = [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
-    return $uploadResponse
-}
-
 Set-StrictMode -Version 3.0 
-
 [String]$MetricName = "DemoMetric"
 [String]$streamName = "Custom-MyTableRawData" #name of the stream in the DCR that represents the destination table
 [String]$WebSite_Time_Zone = "UTC"
 [string]$CommputerName = $env:COMPUTERNAME
 #
-# MIssing check
+# Check for Azure context
 #
 $GetAzContext = Get-AzContext -ErrorAction SilentlyContinue
 if ($null -eq $GetAzContext) {
@@ -90,19 +37,20 @@ if ($null -eq $GetAzContext) {
 }
 #
 # Check if resource Group exists in the subscription
-# OK
+#
 $RG_Object = Get-AzResourceGroup -Name $ResourceGroup_name -ErrorAction SilentlyContinue
 if ($null -eq $RG_Object) {
     [String]$ErrorMessage = "Resource group named $ResourceGroup_name does not exist in subscription $((get-azcontext).Subscription.Name)."
     Write-Error $ErrorMessage
     exit
-} else {
-[String]$Message = "Resource group named $ResourceGroup_name exists in subscription $((get-azcontext).Subscription.Name)."
+}
+else {
+    [String]$Message = "Resource group named $ResourceGroup_name exists in subscription $((get-azcontext).Subscription.Name)."
     Write-Output $Message
 }
 #
 # Extract Data Collection Endpoint API and Data Collection rule ImmutableID from azure resources in the resource group
-# OK
+#
 [string]$dceEndpoint = (get-azresource -resourcegroupname $ResourceGroup_name -ResourceType microsoft.insights/datacollectionendpoints  -ExpandProperties).Properties.logsIngestion.endpoint
 [String]$dcrImmutableId = (get-azresource -resourcegroupname $ResourceGroup_name -ResourceType microsoft.insights/datacollectionrules  -ExpandProperties).Properties.immutableId
 #
@@ -111,19 +59,16 @@ if ($null -eq $RG_Object) {
 [String]$SelectedAzureRegion = (get-azlocation | Select-Object location).location | get-random
 [String]$Message = "Metric to Azure Monitor Log Ingestion API $MetricName = $MetricValue for environment $CommputerName in Region $SelectedAzureRegion."
 Write-Output  $Message
-
-# DEBUG MODE ON
-
-    #
-    # Get Access token from current context
-    #
-    Add-Type -AssemblyName System.Web
-    # Decode SecureString : https://learn.microsoft.com/en-us/powershell/azure/faq?view=azps-13.3.0#how-can-i-convert-a-securestring-to-plain-text-in-powershell-
-    $SecureString = (Get-AzAccessToken  -ResourceUrl "https://monitor.azure.com/" -AsSecureString -WarningAction SilentlyContinue).Token
-    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
-    $BearTokenplainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-    $currentTime = Get-Date ([datetime]::UtcNow) -Format O
-    $staticData = @"
+#
+# Get Access token from current context
+#
+Add-Type -AssemblyName System.Web
+# Decode SecureString : https://learn.microsoft.com/en-us/powershell/azure/faq?view=azps-13.3.0#how-can-i-convert-a-securestring-to-plain-text-in-powershell-
+$SecureString = (Get-AzAccessToken  -ResourceUrl "https://monitor.azure.com/" -AsSecureString -WarningAction SilentlyContinue).Token
+$ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureString)
+$BearTokenplainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+$currentTime = Get-Date ([datetime]::UtcNow) -Format O
+$staticData = @"
 [
 {
     "Time": "$currentTime",
@@ -138,30 +83,16 @@ Write-Output  $Message
 }
 ]
 "@;
-    # https://learn.microsoft.com/en-us/azure/azure-monitor/logs/logs-ingestion-api-overview
-    try {
-        $body = $staticData;
-        $headers = @{"Authorization" = "Bearer $BearTokenplainText"; "Content-Type" = "application/json" }
-        $uri = "$dceEndpoint/dataCollectionRules/$dcrImmutableId/streams/$($streamName)?api-version=2021-11-01-preview"
-        $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers    
-    }
-    catch {
-        [String]$ErrorMessage = "Error while publishing custom metrics using uri $uri : $($_.Exception.Message)."
-        Write-Error $ErrorMessage
-        return $null
-    }
-    $BearTokenplainText = [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
-
-# DEBIG MODE OFF
-
-<#
-$return = Publish_Custom_Metric -CommputerName $CommputerName -AzureRegion $SelectedAzureRegion  -Counter_Name $MetricName -Counter_value $MetricValue -Level 0
-If ($null -ne $return) {
-    [String]$BodyMessage = "Metric successfully published."
-    Write-Output $BodyMessage
+# https://learn.microsoft.com/en-us/azure/azure-monitor/logs/logs-ingestion-api-overview
+try {
+    $body = $staticData;
+    $headers = @{"Authorization" = "Bearer $BearTokenplainText"; "Content-Type" = "application/json" }
+    $uri = "$dceEndpoint/dataCollectionRules/$dcrImmutableId/streams/$($streamName)?api-version=2021-11-01-preview"
+    $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers    
 }
-else {
-    [String]$BodyMessage = "Error while publishing custom metrics."
-    Write-Output $BodyMessage
+catch {
+    [String]$ErrorMessage = "Error while publishing custom metrics using uri $uri : $($_.Exception.Message)."
+    Write-Error $ErrorMessage
+    return $null
 }
-#>
+$BearTokenplainText = [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DemoAMLIAPI
 
-This repo contains demo code associated to articles series related to Azure Monitor Log Ingestion API available on [Cellenza Blog](https://blog.cellenza.com/)
+This repo contains demo code associated to articles series related to Azure Monitor Logs Ingestion API available on [Cellenza Blog](https://blog.cellenza.com/)
 
 - [Introduction](./0-Introduction/) contains introduction related examples
